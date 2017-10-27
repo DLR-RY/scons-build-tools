a8168525c5d6338b8bf74d13d0779a267fe4b0c7
Muhammad Bassam <muhammad.bassam@dlr.de>
2017-10-27 17:32:23 +0200

    [rtems-arm] Add build variable for BSP selection and unaligned access qualifier

--- site_tools/compiler_arm_rtems_gcc.py
+++ site_tools/compiler_arm_rtems_gcc.py
@@ -23,13 +23,17 @@ def generate(env, **kw):
     # 'BSPPATH' shall be provided in the build environment settings
     env.SetDefault(BSPPATH='/opt/arm-rtems4.12/bsp/arm-rtems4.12/') 
     
+    # When compiling the system for a ARMv6 or ARMv7-A/R processors,
+    # the ARM Compiler will assume that it can use unaligned accesses.
+    # for not using unaligned-access then '-mno_unaligned_access flag'
+    # shall be set. (see http://infocenter.arm.com/help/topic/com.arm.doc.faqs/ka15414.html)
+    
     env.SetDefault(CCFLAGS_target=['-march=armv7-a', '-mcpu=cortex-a9', 
                                    '-mthumb', '-mthumb-interwork', '-mfpu=neon', 
-                                   '-mfloat-abi=hard', '-mtune=cortex-a9', 
+                                   '-mfloat-abi=hard', '-mtune=cortex-a9',
                                    '-qrtems', '--specs', 'bsp_specs',
                                    '-B$BSPPATH/lib', 
-                                   '-B$BSPPATH/xilinx_zynq_zedboard/lib',
-                                   '-B$BSPPATH/xilinx_zynq_hpnboard/lib'])
+                                   '-B$BSPPATH/$BSP/lib'])
     env.SetDefault(CCFLAGS_optimize=['-O2', '-ffunction-sections', 
                                      '-fdata-sections'])
 
